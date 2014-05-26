d1976f542a875f1ca8845b4fbaaabec918cd3db3
Maciej Małecki <me@mmalecki.com>
2014-05-26 23:57:20 +0200

    Send requests to powerbar server

--- templates/powerbars.js
+++ templates/powerbars.js
@@ -1,13 +1,37 @@
-(function (exports) {
-  exports.groupOn = function (group) {
-  };
+var Powerbar = window.Powerbar = function (url) {
+  this.url = url;
+};
 
-  exports.groupOff = function (group) {
-  };
+Powerbar.prototype._request = function (path, state) {
+  // For now just fire and mostly (we still log an error, should one occur)
+  // forget an AJAX request.
+  // Our backends accepts form-encoded POST data, `state=On` or `state=Off`.
+  var request = new XMLHttpRequest();
+  var data = 'state=' + encodeURIComponent(state);
+  var url = this.url + path;
 
-  exports.socketOn = function (bar, socketNumber) {
-  };
+  console.log('Requesting: ' + url);
 
-  exports.socketOff = function (bar, socketNumber) {
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  request.open('POST', url, true);
+  request.send(data);
+
+  request.onreadystatechange = function () {
+    if (request.readyState === 4) {
+      // 4 means request success (as one would expect).
+      console.log('Reply: ' + url + ' ' + request.status);
+    }
   };
-})(window.powerbars = {});
+};
+
+Powerbar.prototype.groupOn = function (group) {
+};
+
+Powerbar.prototype.groupOff = function (group) {
+};
+
+Powerbar.prototype.socketOn = function (socket) {
+};
+
+Powerbar.prototype.socketOff = function (socket) {
+};
