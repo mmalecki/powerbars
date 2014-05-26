467049c4689ef96b3011c79f1f4244ee8c73864d
Maciej Małecki <me@mmalecki.com>
2014-05-26 23:40:10 +0200

    Start working on JavaScript for talking to the server

---
+++ templates/powerbars.js
@@ -0,0 +1,13 @@
+(function (exports) {
+  exports.groupOn = function (group) {
+  };
+
+  exports.groupOff = function (group) {
+  };
+
+  exports.socketOn = function (bar, socketNumber) {
+  };
+
+  exports.socketOff = function (bar, socketNumber) {
+  };
+})(window.powerbars = {});
